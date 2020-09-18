6ba996522ad69ee47dde94d2814ca75adbe26844
mushfiqu13 <mushfique13@hotmail.com>
2020-09-18 14:46:52 +0600

    ss

--- lib/main.dart
+++ lib/main.dart
@@ -10,40 +10,7 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.amber,
-        title: Text("Zidan's App"),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Container(
-          //margin: EdgeInsets.fromLTRB(50, 30, 0, 0),
-          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.deepPurpleAccent, width: 10),
-            color: Colors.blueGrey[700],
-          ),
-          child: Text(
-            'Hello maitey!',
-            style: TextStyle(
-              fontSize: 40,
-            ),
-          ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.amber,
-        onPressed: () {},
-        child: Icon(Icons.add),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      bottomNavigationBar: BottomAppBar(
-        shape: CircularNotchedRectangle(),
-        color: Colors.amber,
-        child: Container(
-          height: 50.0,
-        ),
-      ),
+
     );
   }
 }
