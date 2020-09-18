e6cd40e9c4b26cc782bc7a9874924dcb5a3ffd90
mushfiqu13 <mushfique13@hotmail.com>
2020-09-18 15:06:16 +0600

    learned list of data

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,39 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Home(),
+    home: Clubs(),
   ));
 }
 
-class Home extends StatelessWidget {
+class Clubs extends StatelessWidget {
+  List<String> footBallClubs = [
+    'Liverpool FC',
+    'Bayern Munich',
+    'Manchester City',
+    'Juventus',
+    'Real Madrid'
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      backgroundColor: Colors.cyan[200],
+      appBar: AppBar(
+        title: Text(
+          'Top 5 Clubs in the World',
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.purple[900],
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: footBallClubs.map((fc){
+            return Text('$fc');
+          }).toList(),
+        ),
+      ),
     );
   }
 }
+
